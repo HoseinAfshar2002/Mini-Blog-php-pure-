d75539db84fc5645ac01c4d372ab4916b4dfadcb
Hosein Afshar <hosseinafsharnezhad2002@gmail.com>
2025-06-11 18:37:43 +0330

    Commit-1 (Creating a mini blog project and connecting to the database)

---
+++ weblog-project/database/pdo.php
@@ -0,0 +1,12 @@
+<?php
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+try {
+    $connection = new PDO("mysql:host=$serverName;dbname=Weblog", $userName, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "موفق";
+} catch (PDOException $e) {
+    echo "دیتا بیس متصل نیست" . $e->getMessage();
+}
+ 
